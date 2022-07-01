7745bed54c90d46837662314b76e155e361680e6
danianise <danianisehoey@gmail.com>
2022-06-30 21:59:01 -0400

    Removed SocialMedia from models

--- alumni_network_app/admin.py
+++ alumni_network_app/admin.py
@@ -1,3 +1,8 @@
+from re import S
 from django.contrib import admin
+from .models import Network, User, Post, Comment
 
-# Register your models here.
+admin.site.register(Network)
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
--- alumni_network_app/models.py
+++ alumni_network_app/models.py
@@ -7,12 +7,12 @@ class Network(models.Model):
     location = models.CharField(max_length=100)
     logo = models.ImageField(upload_to='logos/')
 
-class SocialMedia(models.Model):
-    linkedin = models.URLField(blank=True, null=True)
-    github = models.URLField(blank=True, null=True)
-    facebook = models.URLField(blank=True, null=True)
-    twitter = models.URLField(blank=True, null=True)
-    instagram = models.URLField(blank=True, null=True)
+# class SocialMedia(models.Model):
+#     linkedin = models.URLField(blank=True, null=True)
+#     github = models.URLField(blank=True, null=True)
+#     facebook = models.URLField(blank=True, null=True)
+#     twitter = models.URLField(blank=True, null=True)
+#     instagram = models.URLField(blank=True, null=True)
 # blank=True and null=True so these fields can be blank
 
 class User(models.Model):
@@ -21,7 +21,12 @@ class User(models.Model):
     password = models.CharField(max_length=30)
     photo = models.ImageField(upload_to='images/')
     location = models.CharField(max_length=100)
-    socialmedia = models.ForeignKey(SocialMedia, null=True, on_delete=models.SET_NULL, related_name='users')
+    # socialmedia = models.ForeignKey(SocialMedia, null=True, on_delete=models.SET_NULL, related_name='users')
+    linkedin = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
     network = models.ForeignKey(Network, on_delete=models.PROTECT, related_name='users')
     # can networks be plural? need option for more than one...
 # on_delete=models.SET_NULL will set this to null if referenced object is deleted
